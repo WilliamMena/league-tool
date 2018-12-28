11eca838467618066d4a0ca1d2d64b732c9aa4ee
WilliamMena <williammena1991@gmail.com>
2018-12-27 20:35:28 -0500

    Migrate previous models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_012844) do
+ActiveRecord::Schema.define(version: 2018_12_28_013322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2018_12_28_012844) do
     t.string "email"
     t.string "password"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
